5a2545d087a53716b6bf96f501afda6399823a74
fnaaz0 <fnaazsayyed@gmail.com>
2026-07-25 01:44:30 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("SBR Super-App Loaded");
 
+    const exploreBtn = document.getElementById("exploreBtn");
+
+    if (exploreBtn) {
+        exploreBtn.addEventListener("click", () => {
+            alert("Welcome to SBR Super-App 🤲");
+        });
+    }
+});
